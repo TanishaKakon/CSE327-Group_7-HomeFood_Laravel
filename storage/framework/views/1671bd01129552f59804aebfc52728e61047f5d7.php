d27cb233a21cdb9e0370d2b210ffaabfb3ebcce6
adiba123 <57524832+adiba123@users.noreply.github.com>
2021-01-11 12:15:15 +0600

    committed

---
+++ storage/framework/views/1671bd01129552f59804aebfc52728e61047f5d7.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH D:\All Softwares new\All Softwares\Xampp\htdocs\otp\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/subcopy.blade.php ENDPATH**/ ?>
