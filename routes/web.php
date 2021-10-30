4013d094d823a768c1731ff298f9ab718454f1e7
Tanisha Kabir <56975985+TanishaKakon@users.noreply.github.com>
2021-10-30 08:12:30 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,16 @@ Route::get('/foodmenu', function () {
     return view('foodmenu');
 });
 
+Route::get('/cartlist', function () {
+    return view('cartlist');
+});
+
+Route::get('/review', function () {
+    return view('review');
+});
+
+
+
 
 
 Route::get('/contact-us', function () {
@@ -36,6 +46,15 @@ Route::get('/contact-us', function () {
 });  
 Route::get('/ordernow', function () {
     return view('ordernow');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/logout', function () {
+    Session::forget('users');
+    return redirect('login');
 });
 
 Route::get('/contact-us','ContactController@contact');
@@ -47,9 +66,14 @@ Route::post('/contact-us', 'ContactController@contactSubmit')->name('contact.sub
 Route::get("/foodmenu",[FoodController::class,'indexFunction']);
 Route::get("detail/{id}",[FoodController::class,'detailFunction']);
 Route::get("search",[FoodController::class,'searchFunction']);
+Route::get("detail/{id}",[FoodController::class,'detailFunction']);
+Route::get("review",[FoodController::class,'reviewListFunction']);
 Route::post("add_to_cart",[FoodController::class,'addToCartFunction']);
+Route::get("header",[FoodController::class,'cartItemFunction']); 
 Route::get("cartlist",[FoodController::class,'cartListFunction']); 
 Route::get("removecart/{id}",[FoodController::class,'removeCartFunction']);
+Route::get("reviewlist",[FoodController::class,'reviewItemFunction']);
+Route::get("createreview",[FoodController::class,'reviewStoreFunction']); 
 Route::get("ordernow",[FoodController::class,'orderNow']); 
 Route::post("orderplace",[FoodController::class,'orderPlace']);
 Route::get("myorders",[FoodController::class,'myOrders']);
